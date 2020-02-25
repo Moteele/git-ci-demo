8b84e6f73890256d1d076804ae8c369b07754071
Daniel Rychlý <xrychly1@fi.muni.cz>
2020-02-25 12:44:35 +0100

    repair fact

--- src/fact.cpp
+++ src/fact.cpp
@@ -1,7 +1,9 @@
 #include "fact.h"
 
 int MyMath::factorial(int n) {
-    int product = 1;
+    if ( n < 0 )
+	return 0;
+    long long product = 1;
     for (int i = n; i > 0; i--) {
         product *= i;
     }
